f0cc3e46fe76a060253c18a4178a4574533dbfc2
Jacob Barrett <jacob.barrett@agcocorp.com>
2019-08-22 10:58:56 -0500

    Add some types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import { users } from './users'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  const user:User = users[0]
+  res.send(user)
 })
 
 app.listen(3030, () => { console.log('app listening on port 3030') })
-
---
+++ src/users.ts
@@ -0,0 +1,16 @@
+const users:User[] = [
+  {
+    name: 'Aidan',
+    email: 'blah',
+  },
+  {
+    name: 'Paul',
+    email: '233'
+  },
+  {
+    name: 'KD',
+    email: 'dfd'
+  }
+]
+
+export { users }
